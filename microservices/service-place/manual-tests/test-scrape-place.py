1d4ec074bea05742ab62ddacba0af57f55e74327
Daniel Saltz <dsaltz190@gmail.com>
2022-09-14 09:16:36 -0400

    add disable-dev-shm-usage flag

--- microservices/service-place/manual-tests/test-scrape-place.py
+++ microservices/service-place/manual-tests/test-scrape-place.py
@@ -13,7 +13,8 @@ print('Scraping reviews for place', placeId, ', URL', url, ', headless: ', headl
 
 options = Options()
 options.headless = headless
-options.add_argument("--no-sandbox")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 time.sleep(3) # could convert this to a "wait until" thing later
