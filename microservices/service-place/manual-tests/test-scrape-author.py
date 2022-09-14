b1c61f0236432eb37584b076fe28db5ad0697310
Daniel Saltz <dsaltz190@gmail.com>
2022-09-14 18:22:18 -0400

    trying again for scraping

--- microservices/service-place/manual-tests/test-scrape-author.py
+++ microservices/service-place/manual-tests/test-scrape-author.py
@@ -16,7 +16,7 @@ options = Options()
 options.headless = headless
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-# options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--remote-debugging-port=9222')
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 time.sleep(3) # could convert this to a "wait until" thing later
